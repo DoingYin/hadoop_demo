3a96528616574d6c2ca291b81478fed5ff2136ae
YinYichang <yichang1776@163.com>
2018-11-03 15:22:51 +0800

    Hadoop的开始学习HelloWorld

--- src/main/java/com/wordcount/WordCount.java
+++ src/main/java/com/wordcount/WordCount.java
@@ -7,5 +7,8 @@ package com.wordcount;
  * @create: 2018/11/03 15:04
  **/
 public class WordCount {
-    
+
+    public static void main(String args[]) {
+        System.out.println("Hello ");
+    }
 }
